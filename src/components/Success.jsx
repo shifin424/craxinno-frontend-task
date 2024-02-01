f7927046f73bf253413d20d3161961087fc02970
Muhmmed Shifin P <muhammedshifin424@gmail.com>
2024-02-01 20:42:13 +0530

    updated the codebase

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,8 +1,8 @@
 
 const Success = () => {
     return (
-        <div className="flex  flex-col justify-center items-center mt-16">
-            <h1 className="text-3xl font-bold mb-2 text-center">Submission Successfull</h1>
+        <div className="flex  flex-col justify-center text-center items-center mt-16">
+            <h1 className="text-3xl font-bold mb-2 justify-center text-center">Submission Successfull</h1>
             <p className="text-md mb-2">Please check your email to review the submitted information.</p>
         </div>
     )
